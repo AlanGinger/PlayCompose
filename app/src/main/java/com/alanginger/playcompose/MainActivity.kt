f795bd58aa10ce9f5e998e9494d2bb7783cb6a03
姜饼小框 <alan.gingersnap@gmail.com>
2021-06-12 16:55:18 +0800

    hello world

--- app/src/main/java/com/alanginger/playcompose/MainActivity.kt
+++ app/src/main/java/com/alanginger/playcompose/MainActivity.kt
@@ -3,46 +3,21 @@ package com.alanginger.playcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-
-import androidx.compose.ui.tooling.preview.Preview
-import com.alanginger.playcompose.ui.theme.PlayComposeTheme
+import androidx.compose.runtime.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PlayComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Text(text = "Hello world!")
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PlayComposeTheme {
-        Greeting("Android")
-    }
-}
-
-@Composable
 fun count() {
-    val counts = remember {
-        mutableStateOf(0)
+    var expanded by remember {
+        mutableStateOf(false)
     }
 }

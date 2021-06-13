111bfc5fa22146d53e6e04ce4cbb82a87c5b4ad1
Alan Ginger <alan.gingersnap@gmail.com>
2021-06-13 16:02:04 +0800

    add content

--- app/src/main/java/com/alanginger/playcompose/MainActivity.kt
+++ app/src/main/java/com/alanginger/playcompose/MainActivity.kt
@@ -3,21 +3,105 @@ package com.alanginger.playcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.alanginger.playcompose.ui.theme.PlayComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Hello world!")
+            MyApp {
+                Content()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        Content()
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    PlayComposeTheme {
+        Surface(color = Color.Yellow) {
+            content()
+
         }
     }
 }
 
 @Composable
-fun count() {
-    var expanded by remember {
-        mutableStateOf(false)
+fun Content() {
+    var count by remember { mutableStateOf(0) }
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(16.dp))
+                .clickable {
+                    count++
+                },
+            contentScale = ContentScale.FillBounds
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "A day wandering through the sandhills " +
+                    "in Shark Fin Cove, and a few of the " +
+                    "sights I saw",
+            style = typography.h6,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(
+            text = "Davenport, California",
+            style = typography.body2
+        )
+        Text(
+            text = "December ${2018 + count}",
+            style = typography.body2
+        )
+        Divider(modifier = Modifier.padding(0.dp, 10.dp), color = Color.Black)
+        ListView()
+    }
+}
+
+@Composable
+fun ListView() {
+    LazyColumn(modifier = Modifier.fillMaxHeight()) {
+        val names: List<String> = List(1000) { "Hello Android #$it" }
+        items(items = names) { name ->
+            Text(
+                text = name,
+                style = typography.body1
+            )
+            Divider(modifier = Modifier.padding(0.dp, 10.dp), color = Color.Black)
+        }
     }
 }
